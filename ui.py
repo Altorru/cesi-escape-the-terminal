1b22f4cd56e3ff30843424ca42f699c7daa46c12
Hugo VIAUD <hugoviaud85@gmail.com>
2026-02-11 14:19:46 +0100

    ♻️ Refactor PassiveUI to implement Observer pattern with notify method for event handling

--- ui.py
+++ ui.py
@@ -1,24 +1,33 @@
 from rich.console import Console
 from rich.panel import Panel
+from abc import ABC, abstractmethod
 
 console = Console()
 
-class PassiveUI:
+class Observer(ABC):
+    @abstractmethod
+    def notify(self, event_type, data):
+        """Mise à jour de l'observateur en fonction de l'événement"""
+        pass
+
+class PassiveUI(Observer):
 
   """============================== PRINTERS =============================="""
-  @staticmethod
-  def print_title():
-    console.clear()
-    console.print(Panel.fit(
-      "[bold red]Escape from the Terminal[/bold red]\n",
-      border_style="red"
-    ))
+
+  def notify(self, event_type, data):
+    if event_type == "title":
+      console.clear()
+      console.print(Panel.fit(
+        "[bold red]Escape from the Terminal[/bold red]\n",
+        border_style="red"
+      ))
+
   """============================== BUILDERS =============================="""
 
   """============================== Colorizer =============================="""
 
 
-class ActiveUI:
+class ActiveUI(Observer):
 
   """============================== PRINTERS =============================="""
 
